819c34b0ee045c47f663632adafbcb8a8095d83b
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2020-07-30 16:20:40 +0700

    Adding Redux to Your React App

--- src/index.js
+++ src/index.js
@@ -3,13 +3,25 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 import GlobalStyles from './styles/globals';
+import configureStore from './store';
+import { Provider } from 'react-redux';
+
 
 const renderApp = () => {
+	const store = configureStore({});
+
+	if(process.env.NODE_ENV !== 'production'){
+		console.log("Initial State ->");
+		console.log(store.getState());
+	}
+
 	ReactDOM.render(
+		<Provider store={store}>
 		<div>
 		  <GlobalStyles />
 		  <App />
-		</div>,
+		</div>
+		</Provider>,
 		document.getElementById('root')
 	);
 };
